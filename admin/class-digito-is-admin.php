a665ecf28c11b7920d127011dd3bc257f4459ab8
digitobanovci <digito.banovci@gmail.com>
2025-03-30 04:52:11 +0200

    Update class-digito-is-admin.php

--- admin/class-digito-is-admin.php
+++ admin/class-digito-is-admin.php
@@ -5,6 +5,8 @@ class Digito_IS_Admin {
         add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
         add_action( 'admin_init', array( $this, 'register_settings' ) );
         add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
+        add_action( 'add_meta_boxes', array( $this, 'add_metabox' ) ); // Dodaj hook za metabox
+        add_action( 'save_post', array( $this, 'save_metabox' ) );     // Dodaj hook za čuvanje
     }
 
     public function add_admin_menu() {
@@ -99,79 +101,78 @@ class Digito_IS_Admin {
         wp_enqueue_media();
         wp_enqueue_script( 'digito-is-admin', plugin_dir_url( __FILE__ ) . 'js/admin.js', array( 'jquery' ), '1.0', true );
     }
-}
 
-// METABOX NA CPT stranicama
-public function add_metabox() {
-    $options = get_option( 'digito_is_options' );
-    $selected_cpts = isset( $options['cpt_select'] ) ? $options['cpt_select'] : array();
+    // METABOX FUNKCIJE UNUTAR KLASE
+    public function add_metabox() {
+        $options = get_option( 'digito_is_options' );
+        $selected_cpts = isset( $options['cpt_select'] ) ? $options['cpt_select'] : array();
 
-    if ( ! empty( $selected_cpts ) ) {
-        foreach ( $selected_cpts as $cpt ) {
-            add_meta_box(
-                'digito_is_metabox',
-                'Instruction Steps',
-                array( $this, 'render_metabox' ),
-                $cpt,
-                'normal',
-                'high'
-            );
+        if ( ! empty( $selected_cpts ) ) {
+            foreach ( $selected_cpts as $cpt ) {
+                add_meta_box(
+                    'digito_is_metabox',
+                    'Instruction Steps',
+                    array( $this, 'render_metabox' ),
+                    $cpt,
+                    'normal',
+                    'high'
+                );
+            }
         }
     }
-    add_action( 'save_post', array( $this, 'save_metabox' ) );
-}
 
-public function render_metabox( $post ) {
-    wp_nonce_field( 'digito_is_save_meta', 'digito_is_nonce' );
-    $steps = get_post_meta( $post->ID, '_digito_is_steps', true );
-    $steps = $steps ? $steps : array();
+    public function render_metabox( $post ) {
+        wp_nonce_field( 'digito_is_save_meta', 'digito_is_nonce' );
+        $steps = get_post_meta( $post->ID, '_digito_is_steps', true );
+        $steps = $steps ? $steps : array();
 
-    echo '<div id="digito-is-steps">';
-    foreach ( $steps as $index => $step ) {
-        $this->render_step_fields( $index, $step );
-    }
-    echo '</div>';
-    echo '<button id="digito-is-add-step" class="button">Add Step</button>';
-}
-
-private function render_step_fields( $index, $step ) {
-    $name = isset( $step['name'] ) ? $step['name'] : '';
-    $description = isset( $step['description'] ) ? $step['description'] : '';
-    $orientation = isset( $step['orientation'] ) ? $step['orientation'] : '';
-    $selector = isset( $step['selector'] ) ? $step['selector'] : '';
-
-    echo '<div class="digito-is-step">';
-    echo '<h4>Step ' . ( $index + 1 ) . '</h4>';
-    echo '<p><label>Step Name</label><br><input type="text" name="digito_is_steps[' . $index . '][name]" value="' . esc_attr( $name ) . '"></p>';
-    echo '<p><label>Description</label><br><textarea name="digito_is_steps[' . $index . '][description]">' . esc_textarea( $description ) . '</textarea></p>';
-    echo '<p><label>Instructor Orientation</label><br>';
-    echo '<select name="digito_is_steps[' . $index . '][orientation]">';
-    $orientations = array( 'top-left', 'top-center', 'top-right', 'bottom-left', 'bottom-center', 'bottom-right' );
-    foreach ( $orientations as $ori ) {
-        $selected = ( $orientation == $ori ) ? 'selected' : '';
-        echo '<option value="' . $ori . '" ' . $selected . '>' . ucfirst( str_replace( '-', ' ', $ori ) ) . '</option>';
-    }
-    echo '</select></p>';
-    echo '<p><label>Element Selector</label><br><input type="text" name="digito_is_steps[' . $index . '][selector]" value="' . esc_attr( $selector ) . '"></p>';
-    echo '<button class="button digito-is-remove-step">Remove Step</button>';
-    echo '</div>';
-}
-
-public function save_metabox( $post_id ) {
-    if ( ! isset( $_POST['digito_is_nonce'] ) || ! wp_verify_nonce( $_POST['digito_is_nonce'], 'digito_is_save_meta' ) ) {
-        return;
-    }
-
-    if ( isset( $_POST['digito_is_steps'] ) ) {
-        $steps = array();
-        foreach ( $_POST['digito_is_steps'] as $step ) {
-            $steps[] = array(
-                'name' => sanitize_text_field( $step['name'] ),
-                'description' => sanitize_textarea_field( $step['description'] ),
-                'orientation' => sanitize_text_field( $step['orientation'] ),
-                'selector' => sanitize_text_field( $step['selector'] ),
-            );
+        echo '<div id="digito-is-steps">';
+        foreach ( $steps as $index => $step ) {
+            $this->render_step_fields( $index, $step );
         }
-        update_post_meta( $post_id, '_digito_is_steps', $steps );
+        echo '</div>';
+        echo '<button id="digito-is-add-step" class="button">Add Step</button>';
+    }
+
+    private function render_step_fields( $index, $step ) {
+        $name = isset( $step['name'] ) ? $step['name'] : '';
+        $description = isset( $step['description'] ) ? $step['description'] : '';
+        $orientation = isset( $step['orientation'] ) ? $step['orientation'] : '';
+        $selector = isset( $step['selector'] ) ? $step['selector'] : '';
+
+        echo '<div class="digito-is-step">';
+        echo '<h4>Step ' . ( $index + 1 ) . '</h4>';
+        echo '<p><label>Step Name</label><br><input type="text" name="digito_is_steps[' . $index . '][name]" value="' . esc_attr( $name ) . '"></p>';
+        echo '<p><label>Description</label><br><textarea name="digito_is_steps[' . $index . '][description]">' . esc_textarea( $description ) . '</textarea></p>';
+        echo '<p><label>Instructor Orientation</label><br>';
+        echo '<select name="digito_is_steps[' . $index . '][orientation]">';
+        $orientations = array( 'top-left', 'top-center', 'top-right', 'bottom-left', 'bottom-center', 'bottom-right' );
+        foreach ( $orientations as $ori ) {
+            $selected = ( $orientation == $ori ) ? 'selected' : '';
+            echo '<option value="' . $ori . '" ' . $selected . '>' . ucfirst( str_replace( '-', ' ', $ori ) ) . '</option>';
+        }
+        echo '</select></p>';
+        echo '<p><label>Element Selector</label><br><input type="text" name="digito_is_steps[' . $index . '][selector]" value="' . esc_attr( $selector ) . '"></p>';
+        echo '<button class="button digito-is-remove-step">Remove Step</button>';
+        echo '</div>';
+    }
+
+    public function save_metabox( $post_id ) {
+        if ( ! isset( $_POST['digito_is_nonce'] ) || ! wp_verify_nonce( $_POST['digito_is_nonce'], 'digito_is_save_meta' ) ) {
+            return;
+        }
+
+        if ( isset( $_POST['digito_is_steps'] ) ) {
+            $steps = array();
+            foreach ( $_POST['digito_is_steps'] as $step ) {
+                $steps[] = array(
+                    'name' => sanitize_text_field( $step['name'] ),
+                    'description' => sanitize_textarea_field( $step['description'] ),
+                    'orientation' => sanitize_text_field( $step['orientation'] ),
+                    'selector' => sanitize_text_field( $step['selector'] ),
+                );
+            }
+            update_post_meta( $post_id, '_digito_is_steps', $steps );
+        }
     }
 }
