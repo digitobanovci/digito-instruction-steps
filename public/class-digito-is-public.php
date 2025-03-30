e5968714bcda40c61f4a2de85226bd7df0d9eeef
digitobanovci <digito.banovci@gmail.com>
2025-03-30 03:46:08 +0200

    Update class-digito-is-public.php

--- public/class-digito-is-public.php
+++ public/class-digito-is-public.php
@@ -1,11 +1,40 @@
 <?php
 class Digito_IS_Public {
 
-    public function enqueue_styles() {
-        wp_enqueue_style( 'digito-is-public', plugin_dir_url( __FILE__ ) . '../assets/css/public.css', array(), DIGITO_IS_VERSION );
+    public function __construct() {
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+        add_action( 'wp_footer', array( $this, 'render_instruction_steps' ) );
+    }
+
+    public function render_instruction_steps() {
+        if ( is_singular() ) {
+            global $post;
+            $steps = get_post_meta( $post->ID, '_digito_is_steps', true );
+            if ( ! empty( $steps ) ) {
+                ?>
+                <div id="digito-is-dialog" style="display:none;">
+                    <div class="digito-is-content">
+                        <img src="" class="digito-is-instructor" alt="Instructor">
+                        <h3 class="digito-is-title"></h3>
+                        <p class="digito-is-description"></p>
+                        <button class="digito-is-prev">Previous</button>
+                        <button class="digito-is-next">Next</button>
+                        <button class="digito-is-finish">Finish</button>
+                    </div>
+                </div>
+                <?php
+            }
+        }
     }
 
     public function enqueue_scripts() {
-        wp_enqueue_script( 'digito-is-public', plugin_dir_url( __FILE__ ) . '../assets/js/public.js', array( 'jquery' ), DIGITO_IS_VERSION, true );
+        wp_enqueue_style( 'digito-is-public', plugin_dir_url( __FILE__ ) . 'css/public.css', array(), '1.0' );
+        wp_enqueue_script( 'digito-is-public', plugin_dir_url( __FILE__ ) . 'js/public.js', array( 'jquery' ), '1.0', true );
+        if ( is_singular() ) {
+            wp_localize_script( 'digito-is-public', 'digitoIS', array(
+                'steps' => get_post_meta( get_the_ID(), '_digito_is_steps', true ),
+                'images' => get_option( 'digito_is_options' )['instructor_images']
+            ) );
+        }
     }
 }
